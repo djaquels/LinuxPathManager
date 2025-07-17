7a415e2e93853faea5eca65924224c1cfd2771fd
Hector <hjacales@gmail.com>
2025-07-17 16:54:27 +0200

    chore: add ssh logic

--- src/main/java/com/github/djaquels/utils/UserRemotePathCommand.java
+++ src/main/java/com/github/djaquels/utils/UserRemotePathCommand.java
@@ -4,9 +4,11 @@ import com.jcraft.jsch.*;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class UserRemotePathCommand implements PathCommand {
     private final String username;
@@ -15,11 +17,58 @@ public class UserRemotePathCommand implements PathCommand {
     private final String password;
     private List<String> pathValues;
 
+    Map<String, String> shellFilters = Map.of(
+			"bash", ":",
+			"zsh", ":",
+    		"fish", " ",
+    		"ksh", ":",
+			"csh", ":",
+			"tcsh", ":"
+		);
+
     public UserRemotePathCommand(String username, String host, int port, String password) {
         this.username = username;
         this.host = host;
         this.port = port;
         this.password = password;
+    }
+
+    private String shellType(Session session, String userName){
+        ChannelExec channel = null;
+        List<String> userInfo;
+        try {
+            channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand("cat /etc/passwd | grep " + userName);
+            channel.setInputStream(null);
+            InputStream input = channel.getInputStream();
+            channel.connect();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String output = reader.readLine();
+            if (output != null && !output.isEmpty()) {
+                // Use ':' as the path separator for Linux/Unix systems
+                userInfo = Arrays.asList(output.split(":"));
+            } else {
+                userInfo = Collections.emptyList();
+            }
+            int userInfoSize = userInfo.size();
+            if( userInfoSize == 0){
+                return "bash";
+            }else{
+                List<String> shellInfo = Arrays.asList(userInfo.get(userInfoSize-1).split("/"));
+                return shellInfo.get(shellInfo.size()-1);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            userInfo = Collections.emptyList();
+        } finally {
+            if (channel != null && channel.isConnected()) {
+                channel.disconnect();
+            }
+        }
+        return "bash";
+
     }
 
     @Override
@@ -28,6 +77,8 @@ public class UserRemotePathCommand implements PathCommand {
         ChannelExec channel = null;
         try {
             session = SSHConnectionUtil.connect(this.username, this.host, this.port, this.password);
+            String userShell = shellType(session, this.username);
+            String separator = shellFilters.get(userShell);
             channel = (ChannelExec) session.openChannel("exec");
             channel.setCommand("echo $PATH");
             channel.setInputStream(null);
@@ -38,7 +89,7 @@ public class UserRemotePathCommand implements PathCommand {
             String output = reader.readLine();
             if (output != null && !output.isEmpty()) {
                 // Use ':' as the path separator for Linux/Unix systems
-                pathValues = Arrays.asList(output.split(":"));
+                pathValues = Arrays.asList(output.split(separator));
             } else {
                 pathValues = Collections.emptyList();
             }
